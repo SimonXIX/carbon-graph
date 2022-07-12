8931898b35c8a6bbce4deeac3319a8bfbf508c2c
Simon <ad7588@coventry.ac.uk>
2022-07-12 14:45:38 +0100

    initial commit

---
+++ web/app/__init__.py
@@ -0,0 +1,24 @@
+# @name: __init__.py
+# @version: 0.1
+# @creation_date: 2022-06-07
+# @license: The MIT License <https://opensource.org/licenses/MIT>
+# @author: Simon Bowie <simon.bowie.19@gmail.com>
+# @purpose: Initialises the app
+# @acknowledgements:
+
+from flask import Flask, render_template, request
+from flask_moment import Moment
+import requests
+import urllib.request, json
+import os
+
+# initiate Moment for datetime functions
+moment = Moment()
+
+app = Flask(__name__)
+
+moment.init_app(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
