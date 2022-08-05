835ab25debfdd7308917c19e177e9f07a7a0dfb7
Simon <ad7588@coventry.ac.uk>
2022-08-05 12:01:07 +0100

    WIP: transferring JSON processing from JavaScript to Python

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -28,7 +28,30 @@ def index():
     # open JSON data
     json_data = open('./app/static/data/data.json')
     data = json.load(json_data)
-    return render_template('index.html', month_limit=month_limit, data=data)
+
+    # get all the month labels
+    month_labels = []
+    for year in data['years']:
+        if "months" in year:
+            for month in year['months']:
+                month_labels.append(month['month'] + " " + year['year'])
+    month_labels = month_labels[-(int(month_limit)):]
+
+    # get all the year labels
+    year_labels = []
+    for year in data['years']:
+        if "months" in year:
+            year_labels.append(year['year'])
+    #
+    # # get all the transportation modes
+    # modes = []
+    # for year in data['years']:
+    #     if "months" in year:
+    #         for month in year['months']:
+    #             modes.append(month.keys())
+    #             modes = list(modes)
+
+    return render_template('index.html', month_limit=month_limit, data=data, month_labels=month_labels, year_labels=year_labels)
 
 @app.route('/what')
 def what():
