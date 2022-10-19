b6c4f47667ac85c536e1a53f90b024e45fa2fc97
Simon <ad7588@coventry.ac.uk>
2022-10-19 16:58:52 +0100

    WIP: expanding to include gas and electricity usage

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -1,5 +1,4 @@
 # @name: __init__.py
-# @version: 0.1
 # @creation_date: 2022-06-07
 # @license: The MIT License <https://opensource.org/licenses/MIT>
 # @author: Simon Bowie <simon.bowie.19@gmail.com>
@@ -14,6 +13,7 @@ import os
 import markdown
 import re
 import random
+from . import data
 
 # initiate Moment for datetime functions
 moment = Moment()
@@ -27,77 +27,19 @@ def index():
     # get month_limit from config file
     month_limit = os.environ.get('MONTH_LIMIT')
 
+    # process personal transportation data
     # open JSON data
-    json_data = open('./app/static/data/data.json')
-    data = json.load(json_data)
+    json_data = open('./app/static/data/transport.json')
+    transport_data = json.load(json_data)
+    transport_processed = data.process_data(transport_data, month_limit)
 
-    # get all the month labels
-    month_labels = []
-    for year in data['years']:
-        if "months" in year:
-            for month in year['months']:
-                month_labels.append(month['month'] + " " + year['year'])
-    #show only the last x months of data (configured in environment file)
-    month_labels = month_labels[-(int(month_limit)):]
+    # process energy data
+    # open JSON data
+    json_data = open('./app/static/data/energy.json')
+    energy_data = json.load(json_data)
+    energy_processed = data.process_data(energy_data, month_limit)
 
-    # get all the year labels
-    year_labels = []
-    for year in data['years']:
-        if "months" in year:
-            year_labels.append(year['year'])
-
-    # get all the modes of transport
-    modes = []
-    for year in data['years']:
-        if "months" in year:
-            for month in year['months']:
-                keys = month.keys()
-                for key in keys:
-                    modes.append(key)
-    # remove duplicates
-    modes = list(dict.fromkeys(modes))
-    # remove month names data
-    modes.remove('month')
-
-    # for each mode of transport, push data into an array and add to month_datasets
-    month_datasets = []
-    for mode in modes:
-        mode_data = []
-        for year in data['years']:
-            if "months" in year:
-                for month in year['months']:
-                    if mode in month:
-                        mode_data.append(month[mode])
-                    else:
-                        mode_data.append('0')
-        #show only the last x months of data (configured in environment file)
-        mode_data = mode_data[-(int(month_limit)):]
-        random_colour = "#" + "%06x" % random.randint(0, 0xFFFFFF)
-        month_dataset = {"label": mode, "data": mode_data, "backgroundColor": random_colour}
-        month_datasets.append(month_dataset)
-
-    # for each mode of transport, sum all the data for each year and add to year_datasets
-    year_datasets = []
-    for mode in modes:
-        mode_data = []
-        year_data = []
-        for year in data['years']:
-            if "months" in year:
-                for month in year['months']:
-                    if mode in month:
-                        mode_data.append(month[mode])
-                    # convert strings to floating-point numbers
-                    mode_floats = [float(x) for x in mode_data]
-                    # sum the list
-                    mode_sum = sum(mode_floats)
-                # add to the array of data for each year and reset mode_data
-                year_data.append(mode_sum)
-                mode_data = []
-        random_colour = "#" + "%06x" % random.randint(0, 0xFFFFFF)
-        year_dataset = {"label": mode, "data": year_data, "backgroundColor": random_colour}
-        year_datasets.append(year_dataset)
-
-    return render_template('index.html', month_limit=month_limit, month_labels=month_labels, year_labels=year_labels, month_datasets=month_datasets, year_datasets=year_datasets)
+    return render_template('index.html', month_limit=month_limit, transport=transport_processed, energy=energy_processed)
 
 @app.route('/what')
 def what():
---
+++ web/app/data.py
@@ -0,0 +1,77 @@
+# @name: data.py
+# @creation_date: 2022-10-19
+# @license: The MIT License <https://opensource.org/licenses/MIT>
+# @author: Simon Bowie <simon.bowie.19@gmail.com>
+# @purpose: Processes data
+# @acknowledgements:
+
+import random
+
+def process_data(data, month_limit):
+    # get all the month labels
+    month_labels = []
+    for year in data['years']:
+        if "months" in year:
+            for month in year['months']:
+                month_labels.append(month['month'] + " " + year['year'])
+    #show only the last x months of data (configured in environment file)
+    month_labels = month_labels[-(int(month_limit)):]
+
+    # get all the year labels
+    year_labels = []
+    for year in data['years']:
+        if "months" in year:
+            year_labels.append(year['year'])
+
+    # get all the modes of transport
+    modes = []
+    for year in data['years']:
+        if "months" in year:
+            for month in year['months']:
+                keys = month.keys()
+                for key in keys:
+                    modes.append(key)
+    # remove duplicates
+    modes = list(dict.fromkeys(modes))
+    # remove month names data
+    modes.remove('month')
+
+    # for each mode of transport, push data into an array and add to month_datasets
+    month_datasets = []
+    for mode in modes:
+        mode_data = []
+        for year in data['years']:
+            if "months" in year:
+                for month in year['months']:
+                    if mode in month:
+                        mode_data.append(month[mode])
+                    else:
+                        mode_data.append('0')
+        #show only the last x months of data (configured in environment file)
+        mode_data = mode_data[-(int(month_limit)):]
+        random_colour = "#" + "%06x" % random.randint(0, 0xFFFFFF)
+        month_dataset = {"label": mode, "data": mode_data, "backgroundColor": random_colour}
+        month_datasets.append(month_dataset)
+
+    # for each mode of transport, sum all the data for each year and add to year_datasets
+    year_datasets = []
+    for mode in modes:
+        mode_data = []
+        year_data = []
+        for year in data['years']:
+            if "months" in year:
+                for month in year['months']:
+                    if mode in month:
+                        mode_data.append(month[mode])
+                    # convert strings to floating-point numbers
+                    mode_floats = [float(x) for x in mode_data]
+                    # sum the list
+                    mode_sum = sum(mode_floats)
+                # add to the array of data for each year and reset mode_data
+                year_data.append(mode_sum)
+                mode_data = []
+        random_colour = "#" + "%06x" % random.randint(0, 0xFFFFFF)
+        year_dataset = {"label": mode, "data": year_data, "backgroundColor": random_colour}
+        year_datasets.append(year_dataset)
+
+    return month_labels, year_labels, month_datasets, year_datasets
